00b916a92bc14f8e6b29998aa138476a135fed90
sstoreyme <scottstorey04@gmail.com>
2021-08-12 16:27:01 +0100

    Added draft of itemframe breaking

--- src/me/polo/creeperexplosion/CreeperExpEvent.java
+++ src/me/polo/creeperexplosion/CreeperExpEvent.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
+import org.bukkit.event.hanging.HangingBreakEvent;
 
 public class CreeperExpEvent implements Listener {
 
@@ -11,6 +13,15 @@ public class CreeperExpEvent implements Listener {
     public void onExp(EntityExplodeEvent e){
         if(e.getEntity().getType() == EntityType.CREEPER){
             e.setCancelled(true);
+        }
+    }
+
+    public void IFBreak(HangingBreakByEntityEvent f){
+        if(f.getEntity().getType() == EntityType.ITEM_FRAME){
+            //not removing the cause, this is what caused the entity to be removed
+            if(f.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION){
+                f.setCancelled(true);
+            }
         }
     }
 }
